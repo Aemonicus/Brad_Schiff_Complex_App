dddee19c9f738ba74ff665258fd393874c217820
Florent VICENTE <vicentegroupe@gmail.com>
2020-04-01 17:15:46 +0200

    post view + setup create post controller + deal with header/footer duplicate

--- router.js
+++ router.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const userController = require("./controllers/userController")
+const postController = require("./controllers/postController")
 
+// user related routes
 router.get("/", userController.home)
 
 router.post("/register", userController.register)
@@ -10,5 +12,7 @@ router.post("/login", userController.login)
 
 router.post("/logout", userController.logout)
 
+// post related routes
+router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
 
 module.exports = router
