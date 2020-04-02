528da8d5207624b5cf00e7397a48b77a4ea44163
Florent VICENTE <vicentegroupe@gmail.com>
2020-04-02 17:42:07 +0200

    profile page with dynamic display of posts

--- router.js
+++ router.js
@@ -5,12 +5,12 @@ const postController = require("./controllers/postController")
 
 // user related routes
 router.get("/", userController.home)
-
 router.post("/register", userController.register)
-
 router.post("/login", userController.login)
-
 router.post("/logout", userController.logout)
+
+// profile related routes
+router.get("/profile/:username", userController.ifUserExists, userController.profilePostsScreen)
 
 // post related routes
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
