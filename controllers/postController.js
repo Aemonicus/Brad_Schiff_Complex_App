5f5d86cd315be638f104d6d2511a5dbacbc1a9b4
Florent VICENTE <vicentegroupe@gmail.com>
2020-04-02 16:22:44 +0200

    find and display author/its avatar for each posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -18,7 +18,7 @@ exports.viewSingle = async function (req, res) {
     let post = await Post.findSingleById(req.params.id)
     res.render("single-post-screen", { post })
   } catch {
-    res.send("404 template will go there")
+    res.render("404")
   }
 
 }
