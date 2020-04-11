3c9911c7e86c2ef17dded67b53d91f28f6f31ee9
Florent VICENTE <vicentegroupe@gmail.com>
2020-04-11 15:06:32 +0200

    registration validation step : username

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -23,7 +23,40 @@ export default class RegistrationForm {
   }
 
   usernameHandler() {
-    alert("username handler ran")
+    this.username.errors = false
+    this.usernameImmediately()
+    clearTimeout(this.username.timer)
+    this.username.timer = setTimeout(() => this.usernameAfterDelay(), 3000)
+  }
+
+  usernameImmediately() {
+    if (this.username.value !== "" && !/^([a-zA-Z0-9]+)$/.test(this.username.value)) {
+      this.showValidationError(this.username, "Username can only contain letters and numbers")
+    }
+
+    if (this.username.value.length > 30) {
+      this.showValidationError(this.username, "Username cannot exceed 30 characters")
+    }
+
+    if (!this.username.errors) {
+      this.hideValidationError(this.username)
+    }
+  }
+
+  hideValidationError(item) {
+    item.nextElementSibling.classList.remove("liveValidateMessage--visible")
+  }
+
+  showValidationError(item, message) {
+    item.nextElementSibling.innerHTML = message
+    item.nextElementSibling.classList.add("liveValidateMessage--visible")
+    item.errors = true
+  }
+
+  usernameAfterDelay() {
+    if (this.username.value.length < 4) {
+      this.showValidationError(this.username, "Username must be at least 4 characters")
+    }
   }
 
   insertValidationElements() {
